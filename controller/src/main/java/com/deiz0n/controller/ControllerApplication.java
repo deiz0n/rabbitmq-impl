208da5dd7b751af68d3cc565f07162636568119f
deiz0n <eduardomacedosilva@outlook.com>
2024-11-03 11:01:39 -0300

    feat: criação do controller-microservice

---
+++ controller/src/main/java/com/deiz0n/controller/ControllerApplication.java
@@ -0,0 +1,13 @@
+package com.deiz0n.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControllerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ControllerApplication.class, args);
+    }
+
+}
---
+++ controller/src/test/java/com/deiz0n/controller/ControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deiz0n.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControllerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
